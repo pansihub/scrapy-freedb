b5ddd2da79739ab8a3cee821b609057150bf4f08
kevenli <pbleester@gmail.com>
2021-06-29 11:15:54 +0800

    v0.1.7

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    version='0.1.6',
+    version='0.1.7',
     name='scrapy-freedb',
     description='scrapy freedb plugin',
     packages=find_packages(exclude=['tests', 'tests.*']),
